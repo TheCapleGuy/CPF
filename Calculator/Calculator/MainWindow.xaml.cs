19bb7c43fb630b645e37ee5582340a19806043de
2intheBush <adrianocaple@gmail.com>
2015-04-22 12:32:11 -0700

    tons of progress

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -33,12 +33,13 @@ namespace Calculator
         string numberValue;
         MathState mathOperation;
         int firstNum, secNum, answer;
-
+        bool StartCalculation;
+        string addHistory;
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
         private void Button_Plus(object sender, RoutedEventArgs e)
         {
             if(numberValue != " ")
@@ -94,29 +95,40 @@ namespace Calculator
         }
         private void Button_Equals(object sender, RoutedEventArgs e)
         {
+
             if (numberValue != " ")
             {
                 secNum = Convert.ToInt32(numberValue);
                 if (mathOperation == MathState.ADDITION)
                 {
+                    addHistory = firstNum.ToString() + " + " +  secNum.ToString();
+
                     answer = firstNum + secNum;
                     numberValue = answer.ToString();
                     calcDisplay.Text = numberValue;
+                    addHistory += " = " + answer;
                 }
                 else if (mathOperation == MathState.SUBTRACTION)
                 {
+                    addHistory = firstNum.ToString() + " - " + secNum.ToString();
+
                     answer = firstNum - secNum;
                     numberValue = answer.ToString();
                     calcDisplay.Text = numberValue;
+                    addHistory += " = " + answer;
                 }
                 else if (mathOperation == MathState.MULTIPLICATION)
                 {
+                    addHistory = firstNum.ToString() + " * " + secNum.ToString();
+
                     answer = firstNum * secNum;
                     numberValue = answer.ToString();
                     calcDisplay.Text = numberValue;
+                    addHistory += " = " + answer;
                 }
                 else if (mathOperation == MathState.DIVISION)
                 {
+                    //divide by zero catch
                     try
                     {
                         answer = firstNum / secNum;
@@ -126,74 +138,165 @@ namespace Calculator
                         Console.WriteLine("Fuck you terry");
                         answer = 0;
                     }
-                    //if(secNum != 0)
-                    //{
-                    //    answer = firstNum / secNum;
-                    //}
-                    //else
-                    //{
-                    //    Console.WriteLine("Fuck you terry");
-                    //}
+                    //print to history log 
+                    addHistory = firstNum.ToString() + " / " + secNum.ToString();
+
+                    //print to text for calc
                     numberValue = answer.ToString();
                     calcDisplay.Text = numberValue;
+                    addHistory += " = " + answer;
                 }
+                StartCalculation = true;
+                History.Text += addHistory + "\n";
             }
             else
             {
                  calcDisplay.Text = numberValue;
+                 History.Text = addHistory;
             }
         }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            if(StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 1;
             calcDisplay.Text = numberValue.ToString();
         }
-
-
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 2;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 3;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 4;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 5;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 6;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 7;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 8;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_9(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 9;
             calcDisplay.Text = numberValue.ToString();
         }
         private void Button_Click_0(object sender, RoutedEventArgs e)
         {
+            if (StartCalculation == true)
+            {
+                numberValue = " ";
+                calcDisplay.Text = numberValue;
+                StartCalculation = false;
+            }
             numberValue += 0;
             calcDisplay.Text = numberValue.ToString();
         }
     }
 }
+
+//if i want to ad ISBN excersice to calculator
+
+//public class ISBN
+//{
+//    public bool ISBNValidation(string ISBNCode)
+//    {
+//        List<int> ISBNResult = new List<int> { };
+//        int i = 10;
+//        int combinedResult = 0;
+//        //multiply all ISBN values by 10 - i
+//        foreach (char ch in ISBNCode)
+//        {
+//            int ISBNNumber = Convert.ToInt32(ch);
+//            ISBNNumber *= 10 - i;
+//            i--;
+//            ISBNResult.Add(ISBNNumber);
+//        }
+//        // add all numbers together and divide by 11
+//        for (int j = 0; j < ISBNResult.Count; j++)
+//        {
+//            combinedResult += ISBNResult[j];
+//        }
+//        if ((combinedResult / 11) % 1 == 0)
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            return false;
+//        }
+//    }
+//}
--- SpriteGenerator/SpriteGenerator/MainWindow.xaml.cs
+++ SpriteGenerator/SpriteGenerator/MainWindow.xaml.cs
@@ -20,16 +20,135 @@ namespace SpriteGenerator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    class CanvasWithBitmap : Canvas
+    {
+        public CanvasWithBitmap()
+        {
+            _image = new BitmapImage(new Uri("C:/Users/adrian.caple/Documents/GitHub/CPF/SpriteGenerator/SpriteGenerator/images/test.png"));
+        }
+        protected override void OnRender(DrawingContext dc)
+        {
+            dc.DrawImage(_image, new Rect(0,0,_image.PixelWidth, _image.PixelHeight));
+        }
+        public BitmapImage _image;
+    }
+
+    public class MyImage : Image
+    {
+
+        public double x { get; set; }
+        public double y { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
+        string fileName;
+        List<MyImage> ImageList = new List<MyImage>();
+        private CanvasPopUp myController = new CanvasPopUp();
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-    }
-    class ImageIO
-    {
-        public OpenFileDialog ODlog = new Microsoft.Win32.OpenFileDialog();
 
+        }
+
+        private void Menu_New(object sender, RoutedEventArgs e)
+        {
+            canvasControl.Children.Clear();
+            myController.Show();
+
+            Console.WriteLine("NEWED");
+        }
+
+        private void Menu_Open(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("OPENED");
+        }
+
+        private void Menu_Save(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("SAVED");
+        }
+
+        private void Menu_SaveAs(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("SAVEDAS");
+        }
+
+        private void Menu_Import(object sender, RoutedEventArgs e)
+        {
+            //create open dialog instance
+            OpenFileDialog ODlog = new OpenFileDialog();
+            //set filter options
+            ODlog.Filter = "All Files (*.*)| *.*";
+            ODlog.FilterIndex = 1;
+            ODlog.Multiselect = true;
+            //call show dialog if clicked ok
+            bool? userClickOk = ODlog.ShowDialog();
+            //check and process if clicked
+            if (userClickOk == true)
+            {
+                //System.IO.Stream fileStream = new System.IO.FileStream(ODlog.FileName, System.IO.FileMode.Open);
+                fileName = ODlog.FileName;
+            }
+            Console.WriteLine("IMPORTED");
+        }
+
+        private void Menu_Exit(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("EXITED");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //create open dialog instance
+            OpenFileDialog ODlog = new OpenFileDialog();
+            //set filter options
+            ODlog.Filter = "All Files (*.*)| *.*";
+            ODlog.FilterIndex = 1;
+            ODlog.Multiselect = true;
+            //call show dialog if clicked ok
+            bool? userClickOk = ODlog.ShowDialog();
+            //check and process if clicked
+            if(userClickOk == true)
+            {
+                //System.IO.Stream fileStream = new System.IO.FileStream(ODlog.FileName, System.IO.FileMode.Open);
+                string fileName = ODlog.FileName; 
+            }
+        }
+
+        private void Click_Picture(object sender, RoutedEventArgs e)
+        {
+            //BitmapImage bmp = new BitmapImage(new Uri("C:/Users/adrian.caple/Documents/GitHub/CPF/SpriteGenerator/SpriteGenerator/images/test.png"));
+           if(fileName != null)
+           {
+               BitmapImage _image = new BitmapImage(new Uri(fileName));
+               MyImage _myImage = new MyImage();
+
+               if (ImageList.Count() == 0)
+               {
+                   _myImage.x = _myImage.y = 0;
+               }
+               else
+               {
+                   _myImage.x = ImageList.Last().x + ImageList.Last().Width;
+                   _myImage.y = ImageList.Last().y;
+                   if (_myImage.x > 1000)
+                   {
+                       _myImage.x = 0;
+                       _myImage.y += ImageList.Last().Height;
+                   }
+               }
+               _myImage.Source = _image;
+               _myImage.Width = _image.Width;
+               _myImage.Height = _image.Height;
+               ImageList.Add(_myImage);
+
+               Canvas.SetTop(_myImage, _myImage.y);
+               Canvas.SetLeft(_myImage, _myImage.x);
+               canvasControl.Children.Add(_myImage);
+           }
+           
+        }
     }
 }
